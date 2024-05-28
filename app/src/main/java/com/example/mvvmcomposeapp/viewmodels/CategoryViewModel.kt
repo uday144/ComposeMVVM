e9f01ab151dc1b44f774a9f4865989fe94ef29dd
uday singh <>
2024-05-29 08:36:01 +1000

    updated gradle version

--- app/src/main/java/com/example/mvvmcomposeapp/viewmodels/CategoryViewModel.kt
+++ app/src/main/java/com/example/mvvmcomposeapp/viewmodels/CategoryViewModel.kt
@@ -2,7 +2,7 @@ package com.example.tweetsy.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.tweetsy.repository.TweetRepository
+import com.example.mvvmcomposeapp.repository.TweetRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
